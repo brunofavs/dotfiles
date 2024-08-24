8356b03acbf2712cf7e09a56ad6722ee21b0c100
brunofavs <bruno.favs@ua.pt>
2024-08-25 00:29:07 +0100

    UI fixes

--- hyprland/.config/hypr/scripts/power_profile_switcher.sh
+++ hyprland/.config/hypr/scripts/power_profile_switcher.sh
@@ -6,7 +6,12 @@ current_profile=$(powerprofilesctl get)
 # Check if the profile is "power-saver"
 if [ "$current_profile" == "power-saver" ]; then
     echo "Power profile is power-saver. Executing command..."
+    powerprofilesctl set balanced
+
+elif [ "$current_profile" == "balanced" ]; then
+    echo "Power profile is balanced. Executing different command..."
     powerprofilesctl set performance
+
 elif [ "$current_profile" == "performance" ]; then
     echo "Power profile is performance. Executing different command..."
     powerprofilesctl set power-saver
