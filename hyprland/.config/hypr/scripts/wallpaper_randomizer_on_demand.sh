0c1ef9e88cc5f94c69950891abc59f4865bbdb99
brunofavs <bruno.favs@ua.pt>
2024-11-08 11:29:52 +0000

    Added random on demand

---
+++ hyprland/.config/hypr/scripts/wallpaper_randomizer_on_demand.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Wallpaper randomizer started"
+
+WALLPAPERS_PATH="$HOME/Pictures/Wallpapers/Minimalist"
+HYPRLOCK_CONF="$HOME/.config/hypr/hyprlock.conf"
+
+# Check if WALLPAPER_PATH exists
+if [ ! -d "$WALLPAPERS_PATH" ]; then
+    echo "Directory $WALLPAPERS_PATH does not exist."
+    exit 1
+fi
+
+
+CHOICE=$(ls "$WALLPAPERS_PATH" | grep -E ".png|.jpg|.jpeg" | shuf -n 1)
+
+NEW_WALLPAPER="$WALLPAPERS_PATH/$CHOICE"
+
+hyprctl hyprpaper unload all
+hyprctl hyprpaper preload   "$NEW_WALLPAPER"
+hyprctl hyprpaper wallpaper ",$NEW_WALLPAPER"
+
+wal -i "$NEW_WALLPAPER"
+
+sed -i -r "s/[^\/]*\.(jpg|png|jpeg)/$CHOICE/g" "$HYPRLOCK_CONF"
+
+
