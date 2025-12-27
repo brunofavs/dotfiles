e22a8dbb89cb19be42903ddd93049c7a6a3b28b0
brunofavs <bruno.favs@ua.pt>
2025-12-27 02:29:36 +0000

    QOL changes to trouble
    it usable now lmao

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -2,32 +2,97 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+      focus = true,
+      modes = {
+        preview_float_errors = {
+            mode = "diagnostics",
+            preview = {
+              type = "float",
+              relative = "editor",
+              border = "rounded",
+              title = "Preview",
+              title_pos = "center",
+              position = { 0, -2 },
+              size = { width = 0.3, height = 0.3 },
+              zindex = 200,
+            },
+            filter = {
+              any = {
+                -- buf = 0, -- current buffer
+                {
+                  severity = vim.diagnostic.severity.ERROR, -- errors only
+                  -- limit to files in the current project
+                  function(item)
+                    return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+                  end,
+                },
+              },
+            },
+        },
+        preview_float_other = {
+            mode = "diagnostics",
+            preview = {
+              type = "float",
+              relative = "editor",
+              border = "rounded",
+              title = "Preview",
+              title_pos = "center",
+              position = { 0, -2 },
+              size = { width = 0.3, height = 0.3 },
+              zindex = 200,
+            },
+            filter = {
+              any = {
+                -- buf = 0, -- current buffer
+                {
+                  severity = {
+                    vim.diagnostic.severity.WARN,
+                    vim.diagnostic.severity.INFO,
+                    vim.diagnostic.severity.HINT,
+                  },
+                  -- limit to files in the current project
+                  function(item)
+                    return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+                  end,
+                },
+              },
+            },
+        }
+      },
   },
   cmd = "Trouble",
   keys = {
     {
       "<leader>xw",
-      "<cmd>Trouble diagnostics toggle<cr>",
+      "<cmd>Trouble preview_float_errors toggle<cr>",
       desc = "Diagnostics (Trouble)",
     },
     {
       "<leader>xd",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      "<cmd>Trouble preview_float_errors toggle filter.buf=0<cr>",
       desc = "Buffer Diagnostics (Trouble)",
     },
+
     {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
+      "<leader>xW",
+      "<cmd>Trouble preview_float_other toggle<cr>",
+      desc = "Diagnostics (Trouble)",
     },
     {
-      "<leader>xq",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
+      "<leader>xD",
+      "<cmd>Trouble preview_float_other toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
     },
+    -- {
+    --   "<leader>xL",
+    --   "<cmd>Trouble loclist toggle<cr>",
+    --   desc = "Location List (Trouble)",
+    -- },
+    -- {
+    --   "<leader>xq",
+    --   "<cmd>Trouble qflist toggle<cr>",
+    --   desc = "Quickfix List (Trouble)",
+    -- },
   },
 
 }
