b38b84f38feab45d90e39c2cb4986926f6650415
brunofavs <bruno.favs@ua.pt>
2026-01-06 19:24:02 +0000

    Fixes for 0.53 hyprland and todo comments in nvim

--- hyprland/.config/hypr/scripts/wallpaper_randomizer_on_demand.sh
+++ hyprland/.config/hypr/scripts/wallpaper_randomizer_on_demand.sh
@@ -11,15 +11,39 @@ if [ ! -d "$WALLPAPERS_PATH" ]; then
     exit 1
 fi
 
+# Check if hyprpaper is running, start it if not
+if ! pgrep -x hyprpaper > /dev/null; then
+    echo "hyprpaper not running, starting it..."
+    hyprpaper &
+    while ! pgrep -x hyprpaper > /dev/null; do
+        sleep 0.1
+    done
+    echo "hyprpaper started successfully"
+fi
+
 
 
 CHOICE=$(ls "$WALLPAPERS_PATH" | grep -E ".png|.jpg|.jpeg" | shuf -n 1)
 
 NEW_WALLPAPER="$WALLPAPERS_PATH/$CHOICE"
 
-hyprctl hyprpaper unload all
-hyprctl hyprpaper preload   "$NEW_WALLPAPER"
-hyprctl hyprpaper wallpaper ",$NEW_WALLPAPER"
+# hyprctl hyprpaper unload all
+# hyprctl hyprpaper preload   "$NEW_WALLPAPER"
+# hyprctl hyprpaper wallpaper ",$NEW_WALLPAPER"
+
+end_time=$((SECONDS + 5))
+while [ $SECONDS -lt $end_time ]; do
+
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload "$NEW_WALLPAPER"
+    output=$(hyprctl hyprpaper wallpaper ",$NEW_WALLPAPER" 2>&1)
+    
+    echo "$output" | grep "running" > /dev/null
+    if [ $? -eq 1 ]; then
+        break
+    fi
+    sleep 0.1
+done
 
 wal -i "$NEW_WALLPAPER"
 
