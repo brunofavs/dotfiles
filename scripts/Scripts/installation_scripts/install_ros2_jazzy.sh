54d2e2bef433c4c2c3eda9d883fdd9048286bf6b
brunofavs <bruno.favs@ua.pt>
2026-04-02 02:26:45 +0100

    minor hyprland togglesplit fix

--- scripts/Scripts/installation_scripts/install_ros2_jazzy.sh
+++ scripts/Scripts/installation_scripts/install_ros2_jazzy.sh
@@ -3,17 +3,17 @@ set -euo pipefail
 
 #  Script should be executed with sudo
 # https://docs.ros.org/en/jazzy/Installation/Ubuntu-Install-Debs.html
-#
-# apt install -y software-properties-common
-# add-apt-repository universe
-#
-# apt update && apt install curl -y
-#
-# export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
-#
-# curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
-#
-# dpkg -i /tmp/ros2-apt-source.deb
+
+apt install -y software-properties-common
+add-apt-repository universe
+
+apt update && apt install curl -y
+
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
+
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+
+dpkg -i /tmp/ros2-apt-source.deb
 
 ###
 
